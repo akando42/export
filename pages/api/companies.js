f62151c32182190e35cac011bb9a487bcf7945de
Mike Do Consulter <hoang.do.2102@gmail.com>
2024-12-11 00:51:05 +0700

    companies profile

---
+++ pages/api/companies.js
@@ -0,0 +1,16 @@
+
+import axios from "axios"
+
+export default async (req, res) => {
+  const url = `https://financialmodelingprep.com/api/v3/profile/BYDIF?apikey=C0nnV4DbDeE3f8x224xsSgP6glPZz5tN`
+  await axios
+    .get(url)
+    .then(({ data }) => {
+      res.status(200).json({ data })
+    })
+    .catch(({ err }) => {
+      res.status(400).json({ err })
+    })
+}
+
+// https://financialmodelingprep.com/api/v3/profile/BYDIF?apikey=C0nnV4DbDeE3f8x224xsSgP6glPZz5tN
